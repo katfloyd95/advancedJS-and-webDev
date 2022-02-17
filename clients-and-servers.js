76e1c2225ea95d9526db79815d8e4d551968a51d
katfloyd95 <katarina.floyd1@gmail.com>
2022-02-17 11:26:00 +0100

    Non Determinism

--- clients-and-servers.js
+++ clients-and-servers.js
@@ -1,2 +1,19 @@
 // CLIENTS & SERVERS
 
+// Non Determinism
+
+// Fundamentally, when we write out code in 1 file and don't do any Requests, such as calculating two numbers together, 
+// our code is considered DETERMINISTIC. It is a very controlled environment
+
+// When add these network Requests, the code becomes NONDETERMINISTIC, because, for example, we run our code twice, once it
+// succeeds, and once it fails. 
+// There can be two different behaviors for different runs, because there are so many different paths that can be taken 
+// for NONDETERMINISTIC code.
+
+// Example:
+// There is always a rate limit to the amount of requests we can call (per hour for example), and this is an example of code
+// being NONDETERMINISTIC
+
+// Callback functions are functions that run in different cases, depending on what happens in the request.
+// We will write these. We will have a Success Function, an Error Function, a way to handle the data that we get back or 
+// lack thereof, as well as notifying the user if there is some sort of error.
